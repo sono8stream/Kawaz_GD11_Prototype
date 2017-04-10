0cfb0785f6b097c060982123c171ab4b1165ac42
SONO <sono888.gamestudio@gmail.com>
2017-04-11 00:42:42 +0900

    トリコミ

---
+++ Assets/Script/Letter.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Letter : MonoBehaviour
+{
+    public int no;//手紙番号
+    public bool onClosing, onClose;
+
+    [SerializeField]
+    string text;
+    [SerializeField]
+    Color color;
+    LettersController letCon;
+    RectTransform rt;
+    float iniX, limX;//y方向位置限界
+    public bool onOpening,onOpen;//タッチ後移動中、タッチ後移動完了状態
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<Image>().color = color;
+        letCon = transform.parent.GetComponent<LettersController>();
+        rt = GetComponent<RectTransform>();
+
+        iniX = rt.anchoredPosition.x;
+        limX = 1080 / 2 + rt.sizeDelta.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onOpening && letCon.ChangeActiveNo(no))
+        {
+            onOpening = false;
+            onOpen = true;
+            letCon.paper.GetComponent<Image>().color = color;
+            letCon.paper.transform.FindChild("Text").GetComponent<Text>().text = text;
+        }
+        if (onOpen)
+        {
+            if (Mathf.Round(rt.anchoredPosition.x - limX) == 0)
+            {
+                letCon.pRt.anchoredPosition = new Vector2(letCon.pLimX, letCon.pRt.anchoredPosition.y);
+                onOpen = false;
+            }
+            else
+            {
+                rt.anchoredPosition += Vector2.right * (limX - rt.anchoredPosition.x) / 2;
+                letCon.pRt.anchoredPosition += Vector2.right * (letCon.pLimX - letCon.pRt.anchoredPosition.x) / 2;
+            }
+        }
+        else if(onClosing)
+        {
+            if (Mathf.Round(rt.anchoredPosition.x - iniX) == 0)
+            {
+                letCon.pRt.anchoredPosition = new Vector2(letCon.pIniX, letCon.pRt.anchoredPosition.y);
+                onClosing = false;
+                onClose = true;
+            }
+            else
+            {
+                rt.anchoredPosition += Vector2.right * (iniX - rt.anchoredPosition.x) / 2;
+                letCon.pRt.anchoredPosition += Vector2.right * (letCon.pIniX - letCon.pRt.anchoredPosition.x) / 2;
+            }
+        }
+    }
+
+    public void OnClick()
+    {
+        if (!onOpen)
+        {
+            onOpening = true;
+        }
+    }
+}
---
+++ Assets/Script/LettersController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LettersController : MonoBehaviour
+{
+    public GameObject paper;
+    public RectTransform pRt;
+    public float pIniX, pLimX;
+
+    [SerializeField]
+    int activeNo;
+
+    Letter[] letters;
+
+    void Awake()
+    {
+        paper = GameObject.Find("LPaper");
+        pRt = paper.GetComponent<RectTransform>();
+
+        pIniX = paper.GetComponent<RectTransform>().anchoredPosition.x;
+        pLimX = -50;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        letters = transform.GetComponentsInChildren<Letter>();
+        SetNumbers();
+        letters[0].OnClick();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void SetNumbers()
+    {
+        for (int i = 0; i < letters.Length; i++)
+        {
+            letters[i].no = i;
+        }
+    }
+
+    public bool ChangeActiveNo(int nextNo)
+    {
+        if (letters[activeNo].onClose)//閉じるまで待つ
+        {
+            letters[activeNo].onClose = false;
+            activeNo = nextNo;
+            return true;
+        }
+        else if (!letters[activeNo].onClosing)
+        {
+            letters[activeNo].onClosing = true;
+        }
+        return false;
+    }
+}
